72a27989c595e998f2557c852addcafd49d6728d
robertwija1033 <74468249+robertwija1033@users.noreply.github.com>
2021-06-10 12:23:40 +0700

    Update Tugas4.java

--- Pertemuan4/Tugas4.java
+++ Pertemuan4/Tugas4.java
@@ -5,7 +5,6 @@ public class Tugas4 {
         char Lanjut = 'y';
         Scanner rw = new Scanner(System.in);
         do{
-            System("cls");
             System.out.print("Masukkan nilai : ");
             double Jari_Jari = rw.nextDouble();
             rw.nextLine();
@@ -14,15 +13,21 @@ public class Tugas4 {
             System.out.println("Kubus : ");
             jari_jari.setKubus(Jari_Jari);
             System.out.println(jari_jari.getKubus());
+            System.out.println();
 
+            System.out.println("=================");
             System.out.println("Tabung di dalam kubus : ");
             jari_jari.setTabung(Jari_Jari);
             System.out.println(jari_jari.getTabung());
-
+            System.out.println();
+            
+            System.out.println("=================");
             System.out.println("Bola di dalam tabung : ");
             jari_jari.setBola(Jari_Jari);
             System.out.println(jari_jari.getBola());
-
+            System.out.println();
+            
+            System.out.println("=================");
             System.out.print("Lanjut ? [y/n] : ");
             Lanjut = rw.next().charAt(0);
             rw.nextLine();
