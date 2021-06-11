fc9b39e4a9c8a2098d6d5bb255a4f7bd259431e3
robertwija1033 <74468249+robertwija1033@users.noreply.github.com>
2021-06-11 17:50:14 +0700

    Add files via upload

---
+++ Pertemuan5/bola.java
@@ -0,0 +1,20 @@
+class bola extends tabung {
+    private double volumeBola, luasBola;
+    
+    bola(double r) {
+        super(r);
+        this.volumeBola = ((double) 4/3) * ((double) 22/7) * (r * r * r);
+        this.luasBola = ((double) 4) * ((double) 22/7) * (r * r);
+    }
+
+    //Membuat Getter
+    public String getBola() {
+        return "volume Bola di dalam Tabung = " + volumeBola + "\nLuas Bola di dalam Tabung = " + luasBola;
+    }
+
+    // Membuat Setter
+    public void setBola(Double r) {
+        this.volumeBola = ((double) 4/3) * ((double) 22/7) * (r * r * r);
+        this.luasBola = ((double) 4) * ((double) 22/7) * (r * r);
+    }
+} 
